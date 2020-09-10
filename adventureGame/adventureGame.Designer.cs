a9a06e02417c2c496f84d1a2e072682c3687d500
Rash Kader <rashidul.kader@gmail.com>
2020-09-10 13:36:19 -0700

    Ch13: Functions, procedures, and methods

--- adventureGame/adventureGame.Designer.cs
+++ adventureGame/adventureGame.Designer.cs
@@ -154,6 +154,7 @@
 			this.btnUseWeapon.TabIndex = 11;
 			this.btnUseWeapon.Text = "Use";
 			this.btnUseWeapon.UseVisualStyleBackColor = true;
+			this.btnUseWeapon.Click += new System.EventHandler(this.btnUseWeapon_Click);
 			// 
 			// btnUsePotion
 			// 
@@ -163,6 +164,7 @@
 			this.btnUsePotion.TabIndex = 12;
 			this.btnUsePotion.Text = "Use";
 			this.btnUsePotion.UseVisualStyleBackColor = true;
+			this.btnUsePotion.Click += new System.EventHandler(this.btnUsePotion_Click);
 			// 
 			// btnNorth
 			// 
@@ -172,6 +174,7 @@
 			this.btnNorth.TabIndex = 13;
 			this.btnNorth.Text = "North";
 			this.btnNorth.UseVisualStyleBackColor = true;
+			this.btnNorth.Click += new System.EventHandler(this.btnNorth_Click);
 			// 
 			// btnEast
 			// 
@@ -181,6 +184,7 @@
 			this.btnEast.TabIndex = 14;
 			this.btnEast.Text = "East";
 			this.btnEast.UseVisualStyleBackColor = true;
+			this.btnEast.Click += new System.EventHandler(this.btnEast_Click);
 			// 
 			// btnSouth
 			// 
@@ -190,6 +194,7 @@
 			this.btnSouth.TabIndex = 15;
 			this.btnSouth.Text = "South";
 			this.btnSouth.UseVisualStyleBackColor = true;
+			this.btnSouth.Click += new System.EventHandler(this.btnSouth_Click);
 			// 
 			// btnWest
 			// 
@@ -199,6 +204,7 @@
 			this.btnWest.TabIndex = 16;
 			this.btnWest.Text = "West";
 			this.btnWest.UseVisualStyleBackColor = true;
+			this.btnWest.Click += new System.EventHandler(this.btnWest_Click);
 			// 
 			// rtbLocation
 			// 
