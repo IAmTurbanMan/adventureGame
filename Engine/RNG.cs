da5a5c46ef478c893eaf42a7621886fcef250eac
Rash Kader <rashidul.kader@gmail.com>
2020-09-10 14:02:00 -0700

    Ch15: Getting random numbers for the game

---
+++ Engine/RNG.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Engine
+{
+	public static class RNG
+	{
+		private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+		public static int NumberBetween(int minValue, int maxValue)
+		{
+			byte[] randomNumber = new byte[1];
+
+			_generator.GetBytes(randomNumber);
+
+			double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+			double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+
+			int range = maxValue - minValue + 1;
+
+			double randomValueInRange = Math.Floor(multiplier * range);
+
+			return (int)(minValue + randomValueInRange);
+		}
+	}
+}
